3b8f9df29e46f292a65576d0b086e0f9fbda8cd8
masrur-sakib <mashrur333@gmail.com>
2024-11-12 22:27:11 +0600

    code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import { GProvider } from './components/Contexts/GContext';
 import TemperaturePrediction from './components/Prediction/TemperaturePrediction';
 import FloodPrediction from './components/Prediction/FloodPrediction';
 
 function App() {
   return (
     <div className='min-h-screen bg-blue-200'>
-      <GProvider>
-        <Routes>
-          <Route path='/' element={<TemperaturePrediction />}></Route>
-          <Route path='/flood-prediction' element={<FloodPrediction />}></Route>
-        </Routes>
-      </GProvider>
+      <Routes>
+        <Route path='/' element={<TemperaturePrediction />}></Route>
+        <Route path='/flood-prediction' element={<FloodPrediction />}></Route>
+      </Routes>
     </div>
   );
 }
